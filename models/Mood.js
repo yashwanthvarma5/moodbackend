02e3af8401aa6fba03da98ed824852b20831d702
yashwanthvarma5 <118748767+yashwanthvarma5@users.noreply.github.com>
2025-07-23 11:53:59 +0530

    Update Mood.js

--- models/Mood.js
+++ models/Mood.js
@@ -1,10 +1,10 @@
-// models/Mood.js
 const mongoose = require("mongoose");
 
 const moodSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   mood: { type: String, required: true },
-  date: { type: Date, required: true },
+  note: { type: String },
+  date: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model("Mood", moodSchema);
